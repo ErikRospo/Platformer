0305f62c08f14999363ab0db274efd2879a40284
ErikRospo <rerik.work@gmail.com>
2022-01-17 11:28:44 -0800

    Added more stuff

--- src/game_canvas_script.js
+++ src/game_canvas_script.js
@@ -1,5 +1,6 @@
 var canvas = document.getElementById("game_canvas");
 var ctx = canvas.getContext("2d");
+var level=0;
 class component {
     constructor(width, height, color, x, y, sX, sY) {
         this.width = width;
@@ -43,13 +44,45 @@ class component {
         };
     }
 };
-var player = new component(10, 30, "blue", 50, 50, 50, 50);
-var enemy = new component(10, 30, "red", 60, 60, 60, 60)
-var
+class player extends component {
+    constructor(width, height, color, x, y, sX, sY) {
+        player.call(width, height, color, x, y, sX, sY);
+        this.tag = "Player";
+        
+        this.update= function () {
+            this.context=ctx;
+            this.context.fillRect=this.color;
+            this.context.fillRect(this.x,this.y,this.width,this.height);
+        }
+    }
+}
+class platform extends component {
+    constructor(width, height, color, x, y) {
+        platform.call(width, height, color, x, y);
+        this.tag = "Platform";
+    }
+}
+class enemy extends component {
+    constructor(width, height, color, x, y, sX, sY) {
+        enemy.call(width, height, color, x, y, sX, sY);
+        this.tag = "Enemy";
+    }
+}
+class EndPoint extends platform {
+    constructor(width,height,color,x,y,sX,sY) {
+        EndPoint.call(width,height,color,x,,y,sX,sY);
+        this.tag="EndPoint";
+    }
+}
 function start() {
     canvas.width = 480;
     canvas.height = 270;
+    createComponents();
 };
+function createComponents() {
+    if level==0;
+        new platform(50,10, "green",0,10)
+}
 function updateEvents() {
 
 };
@@ -59,3 +92,4 @@ function update() {
 function clear() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 };
+onload(start());
